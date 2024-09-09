6d10de26d5e11d6c4d1233600a7d53b7e2d76333
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-09 13:56:31 +0530

    Add UI for adding File or Folder

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -2,17 +2,45 @@ import { useState } from "react";
 
 const Folder = ({ explorer }) => {
   const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+  const handleAddFolder = (e, isFolder) => {
+    e.stopPropagation();
+    setExpand(true);
+    setShowInput({
+      visible: true,
+      isFolder,
+    });
+  };
+  const handleBlur = () => {
+    setShowInput({ ...showInput, visible: false });
+  };
+
   if (explorer.isFolder) {
     return (
       <div style={{ marginTop: 5 }}>
         <div className="folder" onClick={() => setExpand(!expand)}>
           <span>📁{explorer.name}</span>
           <div>
-            <button>Folder+</button>
-            <button>File+</button>
+            <button onClick={(e) => handleAddFolder(e, true)}>Folder +</button>
+            <button onClick={(e) => handleAddFolder(e, false)}>File +</button>
           </div>
         </div>
         <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
+          {showInput.visible && (
+            <div className="inputContainer">
+              <span>{showInput.isFolder ? "📁" : "📄"}</span>
+              <input
+                type="text"
+                className="inputContainer__input"
+                onBlur={handleBlur}
+                autoFocus
+              />
+            </div>
+          )}
+
           {explorer.items.map((exp) => {
             return <Folder explorer={exp} key={exp.id} />;
           })}
